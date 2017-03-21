80f8fabf63c50953efc7198f8588a3bb3c91c6a5
Student User <student@cti.pub.ro>
2017-03-21 11:49:14 +0200

    Rez lab

--- ContactsManager/app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/ContactsManagerActivity.java
+++ ContactsManager/app/src/main/java/ro/pub/cs/systems/eim/lab04/contactsmanager/ContactsManagerActivity.java
@@ -17,19 +17,28 @@ import static android.R.attr.visible;
 
 public class ContactsManagerActivity extends AppCompatActivity {
 
-    EditText name = (EditText) findViewById(R.id.editText_name);
-    EditText phone = (EditText) findViewById(R.id.editText_number);
-    EditText email = (EditText) findViewById(R.id.editText_email);
-    EditText address = (EditText) findViewById(R.id.editText_address);
-    EditText jobTitle = (EditText) findViewById(R.id.editText_jobTitle);
-    EditText company = (EditText) findViewById(R.id.editText_company);
-    EditText website = (EditText) findViewById(R.id.editText_website);
-    EditText im = (EditText) findViewById(R.id.editText_im);
+    EditText nameEditText;
+    EditText phoneEditText;
+    EditText emailEditText;
+    EditText addressEditText;
+    EditText jobTitleEditText;
+    EditText companyEditText;
+    EditText websiteEditText;
+    EditText imEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_manager);
+
+        nameEditText = (EditText) findViewById(R.id.editText_name);
+        phoneEditText = (EditText) findViewById(R.id.editText_number);
+        emailEditText = (EditText) findViewById(R.id.editText_email);
+        addressEditText = (EditText) findViewById(R.id.editText_address);
+        jobTitleEditText = (EditText) findViewById(R.id.editText_jobTitle);
+        companyEditText = (EditText) findViewById(R.id.editText_company);
+        websiteEditText = (EditText) findViewById(R.id.editText_website);
+        imEditText = (EditText) findViewById(R.id.editText_im);
 
         final Button showBtn = (Button) findViewById(R.id.button_show_hide);
         Button saveBtn = (Button) findViewById(R.id.button_save);
@@ -48,43 +57,63 @@ public class ContactsManagerActivity extends AppCompatActivity {
                 }
             }
         });
-    }
 
-    void save(){
-        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
-        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
-        if (name != null) {
-            intent.putExtra(ContactsContract.Intents.Insert.NAME, name);
-        }
-        if (phone != null) {
-            intent.putExtra(ContactsContract.Intents.Insert.PHONE, phone);
-        }
-        if (email != null) {
-            intent.putExtra(ContactsContract.Intents.Insert.EMAIL, email);
-        }
-        if (address != null) {
-            intent.putExtra(ContactsContract.Intents.Insert.POSTAL, address);
-        }
-        if (jobTitle != null) {
-            intent.putExtra(ContactsContract.Intents.Insert.JOB_TITLE, jobTitle);
-        }
-        if (company != null) {
-            intent.putExtra(ContactsContract.Intents.Insert.COMPANY, company);
-        }
-        ArrayList<ContentValues> contactData = new ArrayList<ContentValues>();
-        if (website != null) {
-            ContentValues websiteRow = new ContentValues();
-            websiteRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
-            websiteRow.put(ContactsContract.CommonDataKinds.Website.URL, website);
-            contactData.add(websiteRow);
-        }
-        if (im != null) {
-            ContentValues imRow = new ContentValues();
-            imRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE);
-            imRow.put(ContactsContract.CommonDataKinds.Im.DATA, im);
-            contactData.add(imRow);
-        }
-        intent.putParcelableArrayListExtra(ContactsContract.Intents.Insert.DATA, contactData);
-        startActivity(intent);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameEditText.getText().toString();
+                String phone = phoneEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String address = addressEditText.getText().toString();
+                String jobTitle = jobTitleEditText.getText().toString();
+                String company = companyEditText.getText().toString();
+                String website = websiteEditText.getText().toString();
+                String im = imEditText.getText().toString();
+
+                Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                if (name != null) {
+                    intent.putExtra(ContactsContract.Intents.Insert.NAME, name);
+                }
+                if (phone != null) {
+                    intent.putExtra(ContactsContract.Intents.Insert.PHONE, phone);
+                }
+                if (email != null) {
+                    intent.putExtra(ContactsContract.Intents.Insert.EMAIL, email);
+                }
+                if (address != null) {
+                    intent.putExtra(ContactsContract.Intents.Insert.POSTAL, address);
+                }
+                if (jobTitle != null) {
+                    intent.putExtra(ContactsContract.Intents.Insert.JOB_TITLE, jobTitle);
+                }
+                if (company != null) {
+                    intent.putExtra(ContactsContract.Intents.Insert.COMPANY, company);
+                }
+                ArrayList<ContentValues> contactData = new ArrayList<ContentValues>();
+                if (website != null) {
+                    ContentValues websiteRow = new ContentValues();
+                    websiteRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
+                    websiteRow.put(ContactsContract.CommonDataKinds.Website.URL, website);
+                    contactData.add(websiteRow);
+                }
+                if (im != null) {
+                    ContentValues imRow = new ContentValues();
+                    imRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE);
+                    imRow.put(ContactsContract.CommonDataKinds.Im.DATA, im);
+                    contactData.add(imRow);
+                }
+                intent.putParcelableArrayListExtra(ContactsContract.Intents.Insert.DATA, contactData);
+                startActivity(intent);
+
+            }
+        });
     }
 }
